f6290573271c631c99be804abb2e93c43f351845
krzk426 <krzk426@gmail.com>
2022-04-26 21:30:50 +0200

    config

--- frontend/src/components/room/RoomTile.vue
+++ frontend/src/components/room/RoomTile.vue
@@ -1,17 +1,39 @@
 <template>
-<div>
-  
-</div>
+  <div class="roomBox">
+    <div class="roomTitle" @click="goToRoom(room.id)">
+      {{room.name}}
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-  name: "RoomTitle",
+  name: "RoomTile",
   props: ["room"],
+  methods: {
+    goToRoom(id){
+      console.log(id)
+    }
+  },
 };
 </script>
 
 
 <style scoped lang="scss">
+.roomBox {
+  // -webkit-box-shadow: 8px 2px 29px -3px var(--v-secondary-base);
+  // -moz-box-shadow: 8px 2px 29px -3px var(--v-secondary-base);
+  // box-shadow: 8px 2px 29px -3px var(--v-secondary-base);
+  width: 300px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border: solid 1px black;
+}
 
+ .roomTitle{
+    font-size: 2em;
+    padding: 50px;
+    font-weight: bold;
+  }
 </style>
--- frontend/src/components/room/RoomsList.vue
+++ frontend/src/components/room/RoomsList.vue
@@ -4,26 +4,34 @@
     <div v-for="room in roomsList"
     :key="room.id"
     >
-    {{room}}
+  <RoomTile
+    :room="room"
+  />
     </div>
   </div>
 </template>
 
 <script>
 // import { apiService } from "../common/api.service.js";
-import axios from 'axios'
-import { CSRF_TOKEN } from "../common/csrf_token"
+// import axios from 'axios'
+// import { CSRF_TOKEN } from "../common/csrf_token"
+import RoomTile from './RoomTile.vue'
 
 export default {
   name: "RoomsList",
   props: [],
+  components:{
+    RoomTile
+  },
   data() {
     return {
       roomsList: [{
+        id: 1,
         name: "room 1",
 
       },
       {
+        id: 2,
         name: "room 2",
       }
       ],
@@ -39,16 +47,16 @@ export default {
       //   this.roomsList = data
       // });
 
-      let config = {
-       headers: {
-         'Access-Control-Allow-Origin': '*',
-        'X-CSRFTOKEN': CSRF_TOKEN
-        }
-    }
+      // let config = {
+      //  headers: {
+        //  'Access-Control-Allow-Origin': '*',
+        // 'X-CSRFTOKEN': CSRF_TOKEN
+        // }
+    // }
 
-    return axios.get(`http://127.0.0.1:8000/api/rooms/`, config)
-    .then(console.log("success"))
-    .catch(error => console.log(error));
+    // return axios.get(`http://127.0.0.1:8000/api/rooms/`, config)
+    // .then(console.log("success"))
+    // .catch(error => console.log(error));
     
 
     },
